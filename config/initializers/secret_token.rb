0803ad3df3bfd24a6c57ef3af9748faa404b6c23
Michael Delaney <michael@fusepilot.com>
2011-11-25 00:26:22 -0600

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Convoy::Application.config.secret_token = '5d21ffec2ee5de5702b191d306983144c8c8db3b37f8b2fd2a74c4e218f84ee311a99cb70a90d2cb13856b5352c71184d2836742e9e62408b64ca9d94def63dc'
